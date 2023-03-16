2ddca6039caf8ef1866f86508f321b712371cd35
Nitinjiwnani <jiwnani01@gmail.com>
2023-03-16 13:49:38 +0530

    integration done, and working

---
+++ lib/home_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+class HomeScreen extends StatefulWidget {
+  final WebSocketChannel channel =
+      IOWebSocketChannel.connect("wss://ws.ifelse.io/");
+  HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final TextEditingController _controller = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Web socket integration'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Form(
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    labelText: 'Send message to the server'),
+                controller: _controller,
+              ),
+            ),
+            StreamBuilder(
+              stream: widget.channel.stream,
+              builder: (context, snapshot) {
+                return Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(snapshot.hasData ? '${snapshot.data}' : ''),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: sendData,
+        child: const Icon(Icons.send),
+      ),
+    );
+  }
+
+  void sendData() {
+    if (_controller.text.isNotEmpty) {
+      widget.channel.sink.add(_controller.text);
+    }
+  }
+
+  @override
+  void dispose() {
+    widget.channel.sink.close();
+    // TODO: implement dispose
+    super.dispose();
+  }
+}
